aa256f7a98695dbf7bee07aeea14c8fce82bf9d8
chriscors <70537376+chriscors@users.noreply.github.com>
2023-02-20 16:09:51 -0500

    Adds shuffle, adds Game

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,16 @@
+import random
+
+
 class Game:
-    pass
+    def __init__(self) -> None:
+        self.deck = None
+        self.play = None
+        self.dealer = None
+        self.setup()
+
+    def setup(self):
+        self.deck = Deck()
+        self.deck.add_cards()
 
 
 class Deck:
@@ -7,12 +18,15 @@ class Deck:
     RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 
     def __init__(self) -> None:
-        cards_list = []
+        self.cards_list = []
 
-    def add_cards():
-        for suit in SUITS:
-            for rank in RANKS:
-                cards_list.append(Card(suit, rank))
+    def add_cards(self):
+        for suit in self.SUITS:
+            for rank in self.RANKS:
+                self.cards_list.append(Card(suit, rank))
+
+    def shuffle(self):
+        random.shuffle(self.cards_list)
 
 
 class Card:
@@ -24,5 +38,4 @@ class Card:
         return f'{self.rank} of {self.suit}'
 
 
-deck1 = Deck()
-deck1.
+game = Game()
